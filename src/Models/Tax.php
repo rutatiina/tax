76ed9f3449667ae988e390d2598f480a686930ab
Gilbert Rutatiina <rutatiina@ruginem.org>
2021-09-19 18:44:26 +0300

    Update - tenant scoping - update globals

--- src/Models/Tax.php
+++ src/Models/Tax.php
@@ -4,7 +4,7 @@ namespace Rutatiina\Tax\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\Traits\LogsActivity;
-use App\Scopes\TenantIdScope;
+use Rutatiina\Tenant\Scopes\TenantIdScope;
 
 class Tax extends Model
 {
